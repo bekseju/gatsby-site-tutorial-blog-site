e8db15533c0bbbd342efc2af603f1dd57a16efd2
kbahn <bekseju@gmail.com>
2022-07-17 17:13:06 +0900

    added Layout components, and modified index.js, about.js to apply layout building block

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import Layout from '../components/layout'
 
 const About = () => {
     return (
-        <main>
-        <title>About Me</title>
-        <h1>About Me</h1>
-        <Link to="/">Back to Home</Link>
+    //     <main>
+    //     <title>About Me</title>
+    //     <h1>About Me</h1>
+    //     <Link to="/">Back to Home</Link>
+    //     <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    //   </main>
+    <Layout pageTitle="About Me">
         <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      </main>
+    </Layout>
     )
 }
 
