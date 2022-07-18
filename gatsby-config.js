5e2f215c2d442f52c492183699aafd9c1ad6a69d
kbahn <bekseju@gmail.com>
2022-07-19 00:59:29 +0900

    added gatshby-plugin-image plugin, and then modified gatsby-config.js. Insert StaticImage component in index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   siteMetadata: {
+    title: `내 첫번째 개츠비 사이트`, 
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -14,6 +15,14 @@ const IndexPage = () => {
     // </main>
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      {/* <StaticImage
+              alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+              src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      /> */}
+      <StaticImage
+              alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+              src="../images/clifford.jpg"
+      />
     </Layout>
   )
 }
