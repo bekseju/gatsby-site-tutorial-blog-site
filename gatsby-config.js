5d88650f7bf1996ba507630905b427fc9d5469be
kbahn <bekseju@gmail.com>
2022-07-31 23:43:21 +0900

    Part 5 completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
   ],
 }
