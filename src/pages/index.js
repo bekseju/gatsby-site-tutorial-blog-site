692b0441efe1186097293b0dfa90c01ce40498fe
kbahn <bekseju@gmail.com>
2022-08-06 23:45:42 +0900

    just finished tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,6 @@ const IndexPage = () => {
     // </main>
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      {/* <StaticImage
-              alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-              src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      /> */}
       <StaticImage
               alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
               src="../images/clifford.jpg"
@@ -26,7 +22,7 @@ const IndexPage = () => {
       <StaticImage
               alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
               src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+      />  
     </Layout>
   )
 }
